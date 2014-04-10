53ca6e9937629d38e40a97897cd8afe8c9b8544d
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-04-10 19:25:19 -0400

    Expressions Industry
    Altered calculations, added variables…

--- Expressions_Industry/JS/Exp_Ind.js
+++ Expressions_Industry/JS/Exp_Ind.js
@@ -1,7 +1,7 @@
 // Lindsay Lambert April 8, 2014 Expressions Industry //
 // What is your average profit earned per client each quarter? //
 
-var clientsMonth = prompt("What is the total number of clients for which you worked during the first month?");
+var clientsMonth = prompt("What is the total number of clients for whom you worked during the first month?");
 var costOfServices = prompt("What is the total amount you earned in the first quarter?");
 var clientsPayFee = prompt("How many clients purchased your marketing package during the first quarter?");
 var monthsInQuarter = 3;
@@ -9,5 +9,7 @@ var marketingPackage = 100;
 var clientsBoughtMarketing = clientsPayFee * marketingPackage;
 var totalCostMonth = costOfServices / clientsMonth;
 var totalCostQuarter = totalCostMonth * monthsInQuarter;
-var result = totalCostQuarter + clientsBoughtMarketing;
-console.log("The average profit you earned quarterly was " + result + "dollars per client along with your marketing package sales!")
+var result = clientsBoughtMarketing + totalCostQuarter;
+var resultTotal = totalCostQuarter + clientsBoughtMarketing;
+console.log("The average profit you earned quarterly was " + totalCostQuarter + " dollars per client. With your marketing package sales at " + clientsBoughtMarketing + ", your total earnings for the quarter were " + resultTotal + " dollars!");
+
