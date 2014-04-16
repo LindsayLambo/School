d867ea48c635da48fc7d2af19b116749e76c282b
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-04-16 17:31:57 -0400

    Conditionals Worksheet
    Last Chance For Gas

--- Conditionals_worksheet/JS/script.js
+++ Conditionals_worksheet/JS/script.js
@@ -1,1 +1,13 @@
 // Lindsay Lambert April 15, 2014 Conditionals Worksheet //
+// Last Chance For Gas! //
+
+var mpg = 20;
+var percentInTank = 50%;
+var tankCapacity = 22;
+var gallonsInTank = tankCapacity * percentInTank;
+var milesToGo = gallonsInTank * mpg;
+if(milesToGo < 200){
+	console.log("You only have " + gallonsInTank + " gallons of gas in your tank, so you better get gas now while you can!");
+}else{
+	console.log("Yes, you can make it without stopping for gas!");
+}
