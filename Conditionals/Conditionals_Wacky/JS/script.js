168e2ae86cee430a94780822cf0917915c7498d7
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-04-17 22:52:30 -0400

    Conditionals-Wacky
    completed ternaries…

--- Conditionals/Conditionals_Wacky/JS/script.js
+++ Conditionals/Conditionals_Wacky/JS/script.js
@@ -4,3 +4,8 @@ var semesterInWeeks = 9;
 var numberOfStudents = 2;
 var partyBudgetSemester = 4000;
 var weeklyCostOfAlcohol = prompt("How much do you spend per week on alcohol?");
+var weeklyCostOtherActivities = prompt("How much do you spend each week on other party supplies and activities?");
+var totalWeeklyCost = weeklyCostOfAlcohol + weeklyCostOtherActivities;
+var totalSemesterCost = totalWeeklyCost * semesterInWeeks;
+(totalSemesterCost < partyBudgetSemester) ? console.log("Your partying may continue as you are spending an average of " + totalSemesterCost + " dollars out of your budget of " + partyBudgetSemester + " dollars.") : console.log("You really need to slow down the partying, because you're budget is " + partyBudgetSemester + " dollars and you've been spending an average of " + totalSemesterCost + " dollars!");
+(totalSemesterCost < partyBudgetSemester) ? alert("Your partying may continue as you are spending an average of " + totalSemesterCost + " dollars out of your budget of " + partyBudgetSemester + " dollars.") : alert("You really need to slow down the partying, because you're budget is " + partyBudgetSemester + " dollars and you've been spending an average of " + totalSemesterCost + " dollars!");
