735fd40c9e0a2ba286f4c21dbeb762fc384843d1
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-04-17 22:25:49 -0400

    Conditionals-Wacky
    starting scripting

--- Conditionals/Conditionals_Wacky/JS/script.js
+++ Conditionals/Conditionals_Wacky/JS/script.js
@@ -1,3 +1,6 @@
 // Lindsay Lambert April 17, 2014 Conditionals Wacky //
-// //
-
+// What is the average cost of drinks and extracurricular spending of two college students per 9 week semester and is it over budget? //
+var semesterInWeeks = 9;
+var numberOfStudents = 2;
+var partyBudgetSemester = 4000;
+var weeklyCostOfAlcohol = prompt("How much do you spend per week on alcohol?");
