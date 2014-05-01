41941a8be8da5789058d4f59cc9e0f0985bc6639
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-01 13:37:23 -0400

    Functions Industry
    Trying to come up with good code…drawing a blank…

--- Functions_Industry/JS/script.js
+++ Functions_Industry/JS/script.js
@@ -1,2 +1,4 @@
 // Lindsay Lambert May 1, 2014 Functions Industry //
-//  //
+// How much can you remodel your office building? //
+var remodelBudget = prompt("What is your budget for the office building remodel?");
+var contractorQuote = prompt("How much money, in dollars, did the contractor quote you to complete the remodel?"); 
