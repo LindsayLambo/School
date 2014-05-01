55fd9a1aeffa59cf89d9041b38f8b642b294d5ea
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-01 13:38:30 -0400

    Functions Wacky
    Coming up with decent code, but still trying to figure out a route to
    take…

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -1,2 +1,8 @@
 // Lindsay Lambert May 1, 2014 Functions Wacky //
-//  //
+// How many zombies could you kill in an attack? //
+var numberGuns = prompt("How many guns do you have?");
+var boxesAmmo = prompt("How many boxes of ammunition do you own?");
+var skillLevel = prompt("What is your skill level? Please choose a number between 1 and 10, 10 being expert level.");
+var bulletsFlying = numberGuns * boxesAmmo;
+var avgSkillAndWeapons = bulletsFlying * skillLevel;
+ 
