15f9f3ac7c58f5b2379f1300b446b9fa7923b494
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-01 13:39:35 -0400

    Functions worksheet
    This is difficult to figure out today, for some reason…I will get my
    groove back…

--- Functions_worksheet/JS/Exp_Ind.js
+++
@@ -1,3 +0,0 @@
-// Lindsay Lambert April 24, 2014 Functions Worksheet //
-//  //
-
--- Functions_worksheet/JS/script.js
+++ Functions_worksheet/JS/script.js
@@ -1,8 +1,12 @@
 // Lindsay Lambert April 24, 2014 Functions Worksheet //
 // Circumference //
-function calcCircumference(){
+function calcCircumference(formulaVariable, pi, radius){
 	var radius = 13;
 	var pi = 3.14;
 	var formulaVariable = 2;
-	var circumference = calcCircumference()
+	var circumference = pi * radius;
+	var totalCircumference = circumference * formulaVariable;
+	return totalCircumference;
 }
+calcCircumference(2, 3.14, 13);
+console.log("The circumference is " + totalCircumference + ".");
