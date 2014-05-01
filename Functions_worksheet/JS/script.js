762b2f22ff9b6ce5604f2912b17fb93c70b2428c
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-01 12:24:02 -0400

    Functions Worksheet
    Starting out code and trying to figure out how I’m going to tackle this
    one…

---
+++ Functions_worksheet/JS/script.js
@@ -0,0 +1,8 @@
+// Lindsay Lambert April 24, 2014 Functions Worksheet //
+// Circumference //
+function calcCircumference(){
+	var radius = 13;
+	var pi = 3.14;
+	var formulaVariable = 2;
+	var circumference = calcCircumference()
+}
