1c2694c885e20f6e6640277c57ec380371aad537
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-01 13:37:47 -0400

    Functions Personal
    Trying to come up with good code…drawing a blank…again

--- Functions_Personal/JS/script.js
+++ Functions_Personal/JS/script.js
@@ -1,2 +1,6 @@
 // Lindsay Lambert May 1, 2014 Functions Personal //
-//  //
+// How many months will it take to buy a new car? //
+var monthlyLootTowardCar = prompt("How much money, in dollars, do you save to spend on a new car?");
+var priceOfPotentialCar = prompt("How much are you willing to spend on a new car?");
+var stashedMoney = prompt("Do you have any stashed money that you can put toward your new car fund?");
+var 
