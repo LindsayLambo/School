b9e9dc13c61880d18a493fe45028590a194e7b70
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-03 13:43:43 -0400

    Functions Personal
    Getting it all typed up….hopefully it will work…

--- Functions_Personal/JS/script.js
+++ Functions_Personal/JS/script.js
@@ -1,6 +1,9 @@
 // Lindsay Lambert May 1, 2014 Functions Personal //
 // How many months will it take to buy a new car? //
-var monthlyLootTowardCar = prompt("How much money, in dollars, do you save to spend on a new car?");
+var monthlyLootForCar = prompt("How much money, in dollars, do you save each month to spend on a new car?");
 var priceOfPotentialCar = prompt("How much are you willing to spend on a new car?");
-var stashedMoney = prompt("Do you have any stashed money that you can put toward your new car fund?");
-var 
+var stashedMoney = prompt("How much stashed money do you have that you can put toward your new car fund?");
+var numberMonthsToPay = buyNewCar(monthlyLootForCar, priceOfPotentialCar, stashedMoney);
+function buyNewCar(monthlyLootForCar, priceOfPotentialCar, stashedMoney){
+	var numberMonths = priceOfPotentialCar / monthlyLootForCar;
+}
