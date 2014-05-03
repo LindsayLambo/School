8589a39ca197b664dd1d5883f2bdca4a4638fecf
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-03 15:55:26 -0400

    Functions Industry
    Back and forth with the code…..changes everywhere…cannot fix the
    problem at the moment…

--- Functions_Industry/JS/script.js
+++ Functions_Industry/JS/script.js
@@ -3,7 +3,12 @@
 var remodelBudget = prompt("What is your budget for the office building remodel?");
 var contractorQuoteInside = prompt("How much money, in dollars, did the contractor quote you to complete the interior rooms?"); 
 var contractorQuoteOutside = prompt("What was the estimate the contractor gave you on the exterior of the building?");
+var onlyInterior;
+var onlyExterior;
 var remodelAmount = function(remodelBudget, contractorQuoteInside, contractorQuoteOutside){
 	var totalRemodel = remodelBudget - (contractorQuoteInside + contractorQuoteOutside);
-	
+	console.log(totalRemodel);
 }
+var remodel = remodelAmount(remodelBudget, contractorQuoteInside, contractorQuoteOutside);
+var onlyInterior = remodelBudget - contractorQuoteInside;
+var onlyExterior = remodelBudget - contractorQuoteOutside;
