a53e010a6a56ce26fb141366e6a0ca7bdda9854f
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-03 12:48:34 -0400

    Functions Wacky
    OK. I believe I am finished with the code, now.

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -5,7 +5,7 @@ var boxesAmmo = prompt("How many boxes of ammunition do you own?");
 var skillLevel = prompt("What is your skill level? Please choose a number between 1 and 10, 10 being expert level.");
 var zombiesKilled = bulletsFlying(numberGuns, boxesAmmo, skillLevel);
 function bulletsFlying(numberGuns, boxesAmmo, skillLevel){
-	var zombiesKilled = numberGuns * (boxesAmmo * skillLevel);
+	var zombiesKilled = skillLevel + (numberGuns * boxesAmmo);
 	return zombiesKilled;
 	console.log("The number of zombies you can kill is " + zombiesKilled + ", so come check out my compound when the time comes. I might need you!");
 }
