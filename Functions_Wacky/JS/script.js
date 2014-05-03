8fe7e3ac6e5fe6666ba6bb6d0a96b7c9e8105722
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-03 12:39:03 -0400

    Functions Wacky
    Still trying to get it all working properly…

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -7,5 +7,7 @@ var zombiesKilled = bulletsFlying(numberGuns, boxesAmmo, skillLevel);
 function bulletsFlying(numberGuns, boxesAmmo, skillLevel){
 	var zombiesKilled = numberGuns * (boxesAmmo * skillLevel);
 	return zombiesKilled;
+	console.log("The number of zombies you can kill is " + zombiesKilled + ", so come check out my compound when the time comes. I might need you!");
+	alert("The number of zombies you can kill is " + zombiesKilled + ", so come check out my compound when the time comes. I might need you!");")
 }
  
