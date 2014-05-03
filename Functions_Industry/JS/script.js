9c8b2e32b8198919d6105e85f91c4004c87e3279
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-03 16:52:46 -0400

    Functions Industry
    Still working….

--- Functions_Industry/JS/script.js
+++ Functions_Industry/JS/script.js
@@ -3,12 +3,17 @@
 var remodelBudget = prompt("What is your budget for the office building remodel?");
 var contractorQuoteInside = prompt("How much money, in dollars, did the contractor quote you to complete the interior rooms?"); 
 var contractorQuoteOutside = prompt("What was the estimate the contractor gave you on the exterior of the building?");
-var onlyInterior;
-var onlyExterior;
+
 var remodelAmount = function(remodelBudget, contractorQuoteInside, contractorQuoteOutside){
 	var totalRemodel = remodelBudget - (contractorQuoteInside + contractorQuoteOutside);
-	console.log(totalRemodel);
+	return totalRemodel;
 }
 var remodel = remodelAmount(remodelBudget, contractorQuoteInside, contractorQuoteOutside);
-var onlyInterior = remodelBudget - contractorQuoteInside;
-var onlyExterior = remodelBudget - contractorQuoteOutside;
+(remodelBudget < contractorQuoteInside + contractorQuoteOutside) ? console.log("Congratulations! You can have all your remodeling done!") : console.log("Sorry, but you must choose your next move, because you do not have enough to complete both projects.");
+if(contractorQuoteInside < remodelBudget || contractorQuoteOutside < remodelBudget){
+	console.log("You have plenty of financial backing to complete some remodeling!");	
+}else if(remodelBudget > contractorQuoteInside + contractorQuoteOutside){
+	console.log("You do not have enough money to complete both projects.");
+}else{
+	console.log("You may want to reconsider what exactly you want to remodel and space the work out over a few years.");
+}
