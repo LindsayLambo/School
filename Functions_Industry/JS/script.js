b0e5a1a5b543bb6c5ffae9ab9ece3062cb9a8433
Lindsay Lambert <LindsayLambo@fullsail.edu>
2014-05-03 15:26:52 -0400

    Functions Industry
    Figuring out all the various kinds of twists and turns I am going to
    put in this one.

--- Functions_Industry/JS/script.js
+++ Functions_Industry/JS/script.js
@@ -1,4 +1,9 @@
 // Lindsay Lambert May 1, 2014 Functions Industry //
-// How much can you remodel your office building? //
+// How much will your income allow you to remodel your office building? //
 var remodelBudget = prompt("What is your budget for the office building remodel?");
-var contractorQuote = prompt("How much money, in dollars, did the contractor quote you to complete the remodel?"); 
+var contractorQuoteInside = prompt("How much money, in dollars, did the contractor quote you to complete the interior rooms?"); 
+var contractorQuoteOutside = prompt("What was the estimate the contractor gave you on the exterior of the building?");
+var remodelAmount = function(remodelBudget, contractorQuoteInside, contractorQuoteOutside){
+	var totalRemodel = remodelBudget - (contractorQuoteInside + contractorQuoteOutside);
+	
+}
